310011ebd1fa47099d63f490d4d6e73a1a8b7896
yatamci <yatamci@yahoo.com>
2025-08-04 12:58:26 +0200

    Update script.js
    Button ersetzen

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.getElementById('clickButton').addEventListener('click', () => {
+  const button = document.getElementById('clickButton');
   const msg = document.getElementById('message');
-  msg.classList.remove('hidden');
+
+  button.style.display = 'none';     // 👈 Button wird ausgeblendet
+  msg.classList.remove('hidden');    // 👈 Nachricht wird sichtbar
 });
