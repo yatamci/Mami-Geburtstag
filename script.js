83902c234b31b6731e995991ec5788b3891fb525
yatamci <yatamci@yahoo.com>
2025-08-06 10:54:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const button = document.getElementById('clickButton');
-const msg = document.getElementById('message');
-const bottomImage = document.getElementById('bottomImage');
-const audio = document.getElementById('birthday-audio');
+window.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('clickButton');
+  const msg = document.getElementById('message');
+  const bottomImage = document.getElementById('bottomImage');
+  const audio = document.getElementById('birthday-audio');
 
-button.addEventListener('click', () => {
-  button.style.display = 'none';
-  msg.classList.remove('hidden');
-  bottomImage.classList.remove('hidden'); // 👈 Bild wird eingeblendet
-audio.currentTime = 0;
-  audio.play();
-  
-  confetti({
-    particleCount: 150,
-    spread: 100,
-    origin: { y: 0.6 },
-    emojis: ['🎉','🎂','🎈','💝','🥳','🎊']
+  button.addEventListener('click', () => {
+    button.style.display = 'none';
+    msg.classList.remove('hidden');
+    bottomImage.classList.remove('hidden');
+    
+    audio.currentTime = 0;
+    audio.play();
+
+    confetti({
+      particleCount: 150,
+      spread: 100,
+      origin: { y: 0.6 },
+      emojis: ['🎉','🎂','🎈','💝','🥳','🎊']
+    });
   });
 });
 
